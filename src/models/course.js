598645bdcfecb73bd1ce15fd919227dcd881411e
Giacomo Bergamini <giacomobergamini@MacBook-Air-di-Giacomo.local>
2023-08-24 17:03:50 +0200

    1.1. **Modello 'Course'**:

---
+++ src/models/course.js
@@ -0,0 +1,37 @@
+import Sequelize from 'sequelize'
+
+const Course = (sequelize) => 
+    sequelize.define('course', {
+        courseId: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        description: {
+            type: Sequelize.STRING(1023),
+            allowNull: true,
+            defaultValue: null
+        },
+        startDate: {
+            type: Sequelize.DATEONLY,
+            allowNull: false,
+        },
+        endDate: {
+            type: Sequelize.DATEONLY,
+            allowNull: false,
+        }
+    }, {
+        freezeTableName: true,
+        name: {
+            singular: 'course',
+            plural: 'course'
+        },
+        timestamps: false
+    })
+
+export default Course
